36f3c1d6e4124fca096ec2d7bdfeb4ef59e9762b
gm-15 <gunwoo363@gmail.com>
2025-10-31 00:13:03 +0900

    WebConfig

---
+++ backend/src/main/java/com/insk/insk_backend/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.insk.insk_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // /api/로 시작하는 모든 요청
+                .allowedOrigins("http://localhost:3000") // [중요] 프론트엔드 로컬 개발 서버 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
